0e32b4e948010aa332d8298307d0f4d11b525657
CHAMAKHI <ea65602@actionlogement.fr>
2026-07-23 16:49:55 +0200

    laisser à l'utilisateur la possibilité d'afficher du html dans le header de la simple-table

--- src/elements/simple-table/column.js
+++ src/elements/simple-table/column.js
@@ -11,17 +11,30 @@ import {
 
 /** @typedef {import('aurelia-framework').ViewCompiler} ViewCompiler */
 /** @typedef {import('aurelia-framework').ViewResources} ViewResources */
-/** @typedef {import('aurelia-framework').BehaviorInstruction & {template: InlineViewStrategy}} ViewBehaviorInstruction */
+/**
+ * @typedef {import('aurelia-framework').BehaviorInstruction & {
+ *   template: InlineViewStrategy,
+ *   headerTemplate: InlineViewStrategy,
+ * }} ViewBehaviorInstruction
+ */
 
 /**
- * Creates a template instruction
+ * Creates the cell and header template instructions.
+ * A `<template replace-part="header">` child, if present, defines the header content (allowing
+ * arbitrary html/bindings, e.g. an icon with a bootstrap tooltip). Any other remaining content
+ * defines the cell template, as before.
  * @param {ViewCompiler} _compile unused
  * @param {ViewResources} _resources unused
- * @param {HTMLElement} element the column inner element that defines the cell template
- * @param {ViewBehaviorInstruction} instruction view instruction to enhance in order to provide a cell template
+ * @param {HTMLElement} element the column inner element that defines the header and cell templates
+ * @param {ViewBehaviorInstruction} instruction view instruction to enhance in order to provide the templates
  * @returns {boolean} true indicating that the compiler should also process the content
  */
-function retrieveCellTemplate(_compile, _resources, element, instruction) {
+function retrieveTemplates(_compile, _resources, element, instruction) {
+  const headerElement = element.querySelector(':scope > template[replace-part="header"]');
+  if (headerElement) {
+    instruction.headerTemplate = new InlineViewStrategy(`<template>${headerElement.innerHTML}</template>`);
+    headerElement.remove();
+  }
   const cellTemplate = element.innerHTML;
   if (cellTemplate !== '') {
     instruction.template = new InlineViewStrategy(`<template>${cellTemplate}</template>`);
@@ -34,10 +47,10 @@ function retrieveCellTemplate(_compile, _resources, element, instruction) {
  * @category table
  */
 @noView
-@processContent(retrieveCellTemplate)
+@processContent(retrieveTemplates)
 @inject(TargetInstruction)
 export class Column {
-  /** Header ot the column. @type {string} */
+  /** Header of the column. Ignored if a `<template replace-part="header">` is provided. @type {string} */
   @bindable({ defaultBindingMode: bindingMode.toView })
   header;
 
@@ -62,13 +75,16 @@ export class Column {
   width;
 
   /** Cell template. @type {InlineViewStrategy} */ cellTemplate;
+  /** Header template, allowing arbitrary html content in the column header. @type {InlineViewStrategy} */ headerTemplate;
 
   /**
    * Creates an instance of the `column` custom element.
    * @param {TargetInstruction} targetInstruction instruction for how a target element should be enhanced inside of a view
    */
   constructor(targetInstruction) {
-    this.cellTemplate = /** @type {ViewBehaviorInstruction}*/ (targetInstruction.elementInstruction).template;
+    const elementInstruction = /** @type {ViewBehaviorInstruction}*/ (targetInstruction.elementInstruction);
+    this.cellTemplate = elementInstruction.template;
+    this.headerTemplate = elementInstruction.headerTemplate;
   }
 
   /**
