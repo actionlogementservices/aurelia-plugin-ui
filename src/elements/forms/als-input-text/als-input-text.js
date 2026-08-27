6939386236b1d5e1a2d50aa51ae772bebd169da3
Phuonsavath BOUTAVETH <EL15293@actionlogement.fr>
2026-08-27 17:24:16 +0200

    refacto input text to extend base class

--- src/elements/forms/als-input-text/als-input-text.js
+++ src/elements/forms/als-input-text/als-input-text.js
@@ -1,63 +1,33 @@
 import { bindable, bindingMode, useView } from 'aurelia-framework';
+import { FormInput } from '../forms-input';
 
+/**
+ * @augments FormInput<string>
+ */
 @useView('./als-input-text.html')
-export class AlsInputText {
-  /** Id @type {string} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  // @ts-ignore
-  id;
-
-  /** Name @type {string} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  // @ts-ignore
-  name;
-
+export class AlsInputText extends FormInput {
   /** Type @type {'text' | 'email'} */
   @bindable({ defaultBindingMode: bindingMode.toView })
   type = 'text';
 
-  /** Label @type {string} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  // @ts-ignore
-  label;
+  constructor() {
+    super();
+  }
 
-  /** required @type {boolean} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  required = true;
+  /**
+   * Validate text-specific rules.
+   * @param {string} value Value to validate
+   * @returns {boolean} true if valid, false otherwise
+   */
+  validateValue(value) {
+    if (this.type === 'email' && value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+      this.isError = true;
+      this.errorMessage = "L'adresse email n'est pas valide.";
+      return false;
+    }
 
-  /** Value @type {string} */
-  @bindable({ defaultBindingMode: bindingMode.twoWay })
-  // @ts-ignore
-  value;
-
-  /** Placeholder text @type {string} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  placeholder = '';
-
-  /** Readonly @type {boolean} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  readonly = false;
-
-  /** Disabled @type {boolean} */
-  @bindable({ defaultBindingMode: bindingMode.oneTime })
-  disabled = false;
-
-  /** Field only @type {boolean} */
-  @bindable({ defaultBindingMode: bindingMode.oneTime })
-  fieldOnly = false;
-
-  /** Error @type {boolean} */
-  @bindable({ defaultBindingMode: bindingMode.twoWay })
-  isError = false;
-
-  /** Error message @type {string} */
-  @bindable({ defaultBindingMode: bindingMode.toView })
-  errorMessage = '';
-
-  /** Pristine @type {boolean} */
-  pristine = true;
-
-  constructor() {}
+    return true;
+  }
 
   /**
    * Custom handler to prevent typing anything other than a number or to edit the field.
@@ -65,37 +35,13 @@ export class AlsInputText {
    * @returns {boolean} true to continue processing, false to cancel
    */
   handleKeyDown(event) {
+    this.pristine = false;
+
     if (event.key === 'Enter') {
       this.validate(this.value);
       return true;
     }
 
     return true;
-  }
-
-  handleBlur() {
-    this.validate(this.value);
-
-    return true;
-  }
-
-  /**
-   * Validate value against rules
-   * @param {string} value Value to validate
-   */
-  validate(value) {
-    if (!value && this.required) {
-      this.isError = true;
-      this.errorMessage = `Ce champ est obligatoire.`;
-      return;
-    }
-
-    if (this.type === 'email' && value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
-      this.isError = true;
-      this.errorMessage = `L'adresse email n'est pas valide.`;
-    } else {
-      this.isError = false;
-      this.errorMessage = '';
-    }
   }
 }
