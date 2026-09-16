b5607e0e38e165b7d6f509dc63ce503cd2230fea
Phuonsavath BOUTAVETH <EL15293@actionlogement.fr>
2026-09-16 11:17:45 +0200

    als-pre-login bind footer properties

--- src/layouts/als-pre-login/als-pre-login.js
+++ src/layouts/als-pre-login/als-pre-login.js
@@ -2,8 +2,26 @@ import { bindable, bindingMode, customElement } from 'aurelia-framework';
 
 @customElement('als-pre-login')
 export class AlsPreLogin {
+  /**
+   * URL of the help page.
+   * @type {string}
+   */
   @bindable({ defaultBindingMode: bindingMode.oneTime })
   helpUrl = '#';
+
+  /**
+   * Footer links.
+   * @type {Array<string>}
+   */
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  footerLinks = [];
+
+  /**
+   * Footer type.
+   * @type {'default'|'compact'}
+   */
+  @bindable({ defaultBindingMode: bindingMode.oneTime })
+  footerType = 'compact';
 
   /**
    * Callback function to be executed when the "Se Connecter" button is clicked.
@@ -14,6 +32,9 @@ export class AlsPreLogin {
 
   constructor() {}
 
+  /**
+   * Handles the click event of the "Se Connecter" button.
+   */
   handleButtonClick() {
     if (typeof this.onConnectButtonClick === 'function' && this.onConnectButtonClick) {
       this.onConnectButtonClick();
